ee4f86178dcea0446c57db0b9fe477cb1dd53935
TienPhuc02 <dtp2352002@gmail.com>
2024-01-12 15:11:39 +0700

    desgin home page part3

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,12 @@
-import Header from "@/components/home-page/header/app.header";
+import MainSlider from "@/components/home-page/main-content/main.homepage";
 import * as React from "react";
-
+import { Container } from "@mui/material";
 export default function HomePage() {
-  return <>Home Page</>;
+  return (
+    <>
+      <Container>
+        <MainSlider />
+      </Container>
+    </>
+  );
 }
