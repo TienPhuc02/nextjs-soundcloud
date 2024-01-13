67cd334189f56fc5073d7a273ce2010c7e770ce6
TienPhuc02 <dtp2352002@gmail.com>
2024-01-13 11:01:11 +0700

    fetch data home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
 import MainSlider from "@/components/home-page/main-content/main.homepage";
 import * as React from "react";
 import { Container } from "@mui/material";
-export default function HomePage() {
+export default async function HomePage() {
+  const res = await fetch("http://localhost:8000/api/v1/tracks/top", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      category: "CHILL",
+      limit: 10,
+    }),
+  });
+  console.log("🚀 ~ HomePage ~ res-server:", await res.json());
   return (
     <>
       <Container>
