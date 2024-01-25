8a8d289ec70be317ccebbeb35e20e9713e97c65a
TienPhuc02 <dtp2352002@gmail.com>
2024-01-25 09:00:41 +0700

    wavesurfer hook, usememo

--- src/components/track/wave.track.tsx
+++ src/components/track/wave.track.tsx
@@ -1,29 +1,8 @@
+import { useWavesurfer } from "@/utils/customHook";
 import { useSearchParams } from "next/navigation";
 import React, { useEffect, useMemo, useRef, useState } from "react";
-import WaveSurfer from "wavesurfer.js";
+import WaveSurfer, { WaveSurferOptions } from "wavesurfer.js";
 
-const useWavesurfer = (containerRef: any, options: any) => {
-  const [wavesurfer, setWavesurfer] = useState<any>(null);
-
-  // Initialize wavesurfer when the container mounts
-  // or any of the props change
-  useEffect(() => {
-    if (!containerRef.current) return;
-
-    const ws = WaveSurfer.create({
-      ...options,
-      container: containerRef.current,
-    });
-
-    setWavesurfer(ws);
-
-    return () => {
-      ws.destroy();
-    };
-  }, [options, containerRef]);
-
-  return wavesurfer;
-};
 const WaveTrack = () => {
   const searchParams = useSearchParams();
   const fileName = searchParams.get("audio");
@@ -35,26 +14,7 @@ const WaveTrack = () => {
       url: `/api?audio=${fileName}`,
     };
   }, []);
-  // const options = {
-  //   waveColor: "rgb(200, 0, 200)",
-  //   progressColor: "rgb(100, 0, 100)",
-  //   url: `/api?audio=${fileName}`,
-  // };
   const wavesurfer = useWavesurfer(containerRef, optionsMemo);
-  // useEffect(() => {
-  //   if (containerRef.current) {
-  //     const wavesurfer = WaveSurfer.create({
-  //       container: containerRef.current,
-  //       waveColor: "rgb(200, 0, 200)",
-  //       progressColor: "rgb(100, 0, 100)",
-  //       url: `/api?audio=${fileName}`,
-  //     });
-
-  //     return () => {
-  //       wavesurfer.destroy();
-  //     };
-  //   }
-  // }, []);
   return <div ref={containerRef}>WaveTrack</div>;
 };
 
--- src/utils/customHook.ts
+++ src/utils/customHook.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-
+import WaveSurfer, { WaveSurferOptions } from "wavesurfer.js";
 export const useHasMounted = () => {
   const [hasMounted, setHasMounted] = useState<boolean>(false);
   useEffect(() => {
@@ -22,4 +22,26 @@ export const useScript = (url: string) => {
       document.body.removeChild(script);
     };
   }, [url]);
+};
+export const useWavesurfer = (
+  containerRef: React.RefObject<HTMLDivElement>,
+  options: Omit<WaveSurferOptions, "container">
+) => {
+  const [wavesurfer, setWavesurfer] = useState<any>(null);
+  useEffect(() => {
+    if (!containerRef.current) return;
+
+    const ws = WaveSurfer.create({
+      ...options,
+      container: containerRef.current,
+    });
+
+    setWavesurfer(ws);
+
+    return () => {
+      ws.destroy();
+    };
+  }, [options, containerRef]);
+
+  return wavesurfer;
 };
