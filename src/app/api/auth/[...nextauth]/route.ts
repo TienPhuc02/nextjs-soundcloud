b1c309878d2088db059f9633420a596298c8d385
TienPhuc02 <dtp2352002@gmail.com>
2024-05-06 21:13:25 +0700

    customize session part4

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,8 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import { AuthOptions } from "next-auth";
+import { sendRequest } from "@/utils/api";
+import { JWT } from "next-auth/jwt";
 export const authOptions: AuthOptions = {
   secret: process.env.NO_SECRET,
   // Configure one or more authentication providers
@@ -13,14 +15,30 @@ export const authOptions: AuthOptions = {
     //callback là gọi lại 1 hàm sau khi login thành công
   ],
   callbacks: {
-    jwt({ token, trigger, user, profile, account }) {
+    async jwt({ token, trigger, user, profile, account }) {
       if (trigger === "signIn" && account?.provider === "github") {
-        
+        const res = await sendRequest<IBackendRes<JWT>>({
+          url: "http://localhost:8000/api/v1/auth/social-media",
+          method: "POST",
+          body: {
+            type: "GITHUB",
+            username: user.email,
+          },
+        });
+        if (res.data) {
+          token.access_token = res.data?.access_token;
+          token.refresh_token = res.data?.refresh_token;
+          token.user = res.data.user;
+        }
       }
       return token;
     },
     session({ session, token, user }) {
-      
+      if (token) {
+        session.access_token = token.access_token;
+        session.refresh_token = token.refresh_token;
+        session.user = token.user;
+      }
       return session;
     },
   },
