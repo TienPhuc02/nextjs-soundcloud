fec01eec7a14956eb1527351b0b7d365a01b0152
TienPhuc02 <dtp2352002@gmail.com>
2024-05-06 19:52:47 +0700

    config session for server

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,13 @@ import MainSlider from "@/components/home-page/main-content/main.homepage";
 import * as React from "react";
 import { Container } from "@mui/material";
 import { sendRequest } from "@/utils/api";
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
 export default async function HomePage() {
+  //dựa vào thông tin của người dùng để xem có call API hay không -> thông tin người dùng được lưu trong session -> get session
+
+  const session = await getServerSession(authOptions);
+  console.log(">> check session:", session);
   const chills = await sendRequest<IBackendRes<ITrackTop[]>>({
     url: "http://localhost:8000/api/v1/tracks/top",
     method: "POST",
