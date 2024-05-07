6e98b11d598997f24c763b481d50d541679ac44b
TienPhuc02 <dtp2352002@gmail.com>
2024-05-07 08:53:26 +0700

    login with credentials

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,10 +3,43 @@ import GithubProvider from "next-auth/providers/github";
 import { AuthOptions } from "next-auth";
 import { sendRequest } from "@/utils/api";
 import { JWT } from "next-auth/jwt";
+import CredentialsProvider from "next-auth/providers/credentials";
 export const authOptions: AuthOptions = {
   secret: process.env.NO_SECRET,
   // Configure one or more authentication providers
   providers: [
+    CredentialsProvider({
+      // The name to display on the sign in form (e.g. "Sign in with...")
+      name: "Credentials",
+      // `credentials` is used to generate a form on the sign in page.
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      // You can pass any HTML attribute to the <input> tag through the object.
+      credentials: {
+        username: { label: "Tên đăng nhập", type: "text" },
+        password: { label: "Mật khẩu", type: "password" },
+      },
+      async authorize(credentials, req) {
+        const res = await sendRequest<IBackendRes<JWT>>({
+          url: "http://localhost:8000/api/v1/auth/login",
+          method: "POST",
+          body: {
+            username: credentials?.username,
+            password: credentials?.password,
+          },
+        });
+        // Add logic here to look up the user from the credentials supplied
+        if (res && res.data) {
+          // Any object returned will be saved in `user` property of the JWT
+          return res.data as any;
+        } else {
+          // If you return null then an error will be displayed advising the user to check their details.
+          return null;
+
+          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
+        }
+      },
+    }),
     GithubProvider({
       clientId: process.env.GITHUB_ID!,
       clientSecret: process.env.GITHUB_SECRET!,
@@ -16,12 +49,12 @@ export const authOptions: AuthOptions = {
   ],
   callbacks: {
     async jwt({ token, trigger, user, profile, account }) {
-      if (trigger === "signIn" && account?.provider === "github") {
+      if (trigger === "signIn" && account?.provider !== "credentials") {
         const res = await sendRequest<IBackendRes<JWT>>({
           url: "http://localhost:8000/api/v1/auth/social-media",
           method: "POST",
           body: {
-            type: "GITHUB",
+            type: account?.provider?.toLocaleUpperCase(),
             username: user.email,
           },
         });
@@ -31,6 +64,15 @@ export const authOptions: AuthOptions = {
           token.user = res.data.user;
         }
       }
+      if (trigger === "signIn" && account?.provider === "credentials") {
+        //@ts-ignore
+        token.access_token = user?.access_token;
+        //@ts-ignore
+        token.refresh_token = user?.refresh_token;
+        //@ts-ignore
+        token.user = user.user;
+      }
+      //token với đăng nhập credential
       return token;
     },
     session({ session, token, user }) {
