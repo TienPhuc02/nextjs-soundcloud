619374128a8221ffe17519417ea3ccd5e622467e
TienPhuc02 <dtp2352002@gmail.com>
2024-05-14 15:56:38 +0700

    API upload signle file with fetch

--- src/components/track/tabs/pageTab1.tsx
+++ src/components/track/tabs/pageTab1.tsx
@@ -5,6 +5,8 @@ import { FileWithPath, useDropzone } from "react-dropzone";
 import { styled } from "@mui/material/styles";
 import Button from "@mui/material/Button";
 import CloudUploadIcon from "@mui/icons-material/CloudUpload";
+import { sendRequest, sendRequestFile } from "@/utils/api";
+import { useSession } from "next-auth/react";
 type Props = {};
 const VisuallyHiddenInput = styled("input")({
   clip: "rect(0 0 0 0)",
@@ -33,12 +35,37 @@ function InputFileUpload() {
   );
 }
 const PageTab1 = (props: Props) => {
-  const onDrop = useCallback((acceptedFiles: FileWithPath[]) => {
-    // Do something with the files
-    console.log(">> check accepted File ", acceptedFiles);
-  }, []);
+  const { data: session } = useSession();
+  //useMemo -> variable
+  const onDrop = useCallback(
+    async (acceptedFiles: FileWithPath[]) => {
+      // Do something with the files
+      if (acceptedFiles && acceptedFiles[0]) {
+        const audio = acceptedFiles[0];
+        console.log(" check audio", audio);
+        const formData = new FormData();
+        formData.append("fileUpload", audio);
+        const res = await sendRequestFile<IBackendRes<ITrackTop[]>>({
+          url: "http://localhost:8000/api/v1/files/upload",
+          method: "POST",
+          body: formData,
+          headers: {
+            Authorization: `Bearer ${session?.access_token}`,
+            target_type: "tracks",
+          },
+        });
+        console.log(">> check accepted File ", audio);
+        console.log(">> check session", session?.access_token);
+        console.log("check res", res);
+      }
+    },
+    [session]
+  );
   const { acceptedFiles, getRootProps, getInputProps } = useDropzone({
     onDrop,
+    accept: {
+      audio: [".mp3", ".mp4", ".wav", ".m4a"],
+    },
   });
   const files = acceptedFiles.map((file: FileWithPath) => (
     <li key={file.path}>
