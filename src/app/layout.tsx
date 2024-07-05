bfb0219df6ff6d9fb958a714cb5abf88c52f50b1
TienPhuc02 <dtp2352002@gmail.com>
2024-07-05 14:36:48 +0700

    nextjs local image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,8 @@ import Footer from "@/components/home-page/footer/app.footer";
 import NextAuthWrapper from "@/lib/wrapper.next-auth";
 import { ToastProvider } from "@/utils/toast";
 import { TrackContextProvider } from "@/lib/track.wrapper";
+import Image from "next/image";
+import tienphuc from "../../public/flowers.jpg";
 export default function RootLayout({
   children,
 }: {
@@ -14,13 +16,22 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <ThemeRegistry>
+        <Image
+          src={tienphuc}
+          alt="test next image"
+          sizes="100vw"
+          style={{
+            width: "100%",
+            height: "auto",
+          }}
+        />
+        {/* <ThemeRegistry>
           <NextAuthWrapper>
             <ToastProvider>
               <TrackContextProvider>{children}</TrackContextProvider>
             </ToastProvider>
           </NextAuthWrapper>
-        </ThemeRegistry>
+        </ThemeRegistry> */}
       </body>
     </html>
   );
