c3e5038ba36c33ba2c4d09ed19167014d52b1a79
TienPhuc02 <dtp2352002@gmail.com>
2024-07-05 08:37:43 +0700

    Đặt tên title/description static cho web page

--- src/app/(user)/layout.tsx
+++ src/app/(user)/layout.tsx
@@ -2,7 +2,12 @@ import * as React from "react";
 
 import Header from "@/components/home-page/header/app.header";
 import Footer from "@/components/home-page/footer/app.footer";
+import { Metadata } from "next";
 
+export const metadata: Metadata = {
+  title: "NextJS SoundCloud",
+  description:"miêu tả layout tiêu đề."
+}
 export default function RootLayout({
   children,
 }: {
--- src/app/(user)/like/page.tsx
+++ src/app/(user)/like/page.tsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import { Metadata } from "next";
+import React from "react";
 
+export const metadata: Metadata = {
+  title: "Phuc Like Page",
+  description: "Just Like Page",
+};
 const LikePage = () => {
-    return (
-        <div>
-           LikePage 
-        </div>
-    );
+  return <div>LikePage</div>;
 };
 
 export default LikePage;
