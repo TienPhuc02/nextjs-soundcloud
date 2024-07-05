3cd1b7b2807374db9ade55cf955db0049d71087d
TienPhuc02 <dtp2352002@gmail.com>
2024-07-05 15:55:42 +0700

    next-progress bar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,7 @@ import Footer from "@/components/home-page/footer/app.footer";
 import NextAuthWrapper from "@/lib/wrapper.next-auth";
 import { ToastProvider } from "@/utils/toast";
 import { TrackContextProvider } from "@/lib/track.wrapper";
+import NProgressWrapper from "@/lib/nprogress.wrapper";
 
 export default function RootLayout({
   children,
@@ -16,11 +17,13 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <ThemeRegistry>
-          <NextAuthWrapper>
-            <ToastProvider>
-              <TrackContextProvider>{children}</TrackContextProvider>
-            </ToastProvider>
-          </NextAuthWrapper>
+          <NProgressWrapper>
+            <NextAuthWrapper>
+              <ToastProvider>
+                <TrackContextProvider>{children}</TrackContextProvider>
+              </ToastProvider>
+            </NextAuthWrapper>
+          </NProgressWrapper>
         </ThemeRegistry>
       </body>
     </html>
